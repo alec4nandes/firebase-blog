104fcae2af8890a40eea2967342dd01cecbdc431
Alec <alec4nandes@gmail.com>
2023-05-16 20:43:26 -0700

    build basic game logic

--- public/game/game.js
+++ public/game/game.js
@@ -7,16 +7,31 @@ document.addEventListener("dragover", (e) => {
 let innerX = 0,
     innerY = 0;
 
-const box = document.querySelector("#box");
+let animals = [...document.querySelectorAll(".animal")],
+    gameIsOver = false;
 
-// for mobile
-box.ontouchmove = handleMove;
-box.ontouchstart = handleStart;
-box.ontouchend = handleEnd;
-// for desktop
-box.ondrag = handleMove;
-box.onmousedown = handleStart;
-box.onmouseup = handleEnd;
+animals.forEach((animal) => {
+    // for mobile
+    animal.ontouchmove = handleMove;
+    animal.ontouchstart = handleStart;
+    animal.ontouchend = handleEnd;
+    // for desktop
+    animal.ondrag = handleMove;
+    animal.onmousedown = handleStart;
+    animal.onmouseup = handleEnd;
+    // increase speed over time
+    let count = 1,
+        animalInterval = moveAnimal(animal, 20),
+        interval = setInterval(() => {
+            clearInterval(animalInterval);
+            if (gameIsOver) {
+                clearInterval(interval);
+            } else {
+                animalInterval = moveAnimal(animal, 20 - count * 2);
+                count++;
+            }
+        }, 5000);
+});
 
 function getTouches(e) {
     return e.targetTouches?.[0];
@@ -24,15 +39,16 @@ function getTouches(e) {
 
 function handleMove(e) {
     // grab the location of touch
-    const touches = getTouches(e);
-    // assign box new coordinates based on the touch.
+    const touches = getTouches(e),
+        animal = e.target;
+    // assign animal new coordinates based on the touch.
     if (touches) {
-        box.style.left = touches.pageX - innerX + "px";
-        box.style.top = touches.pageY - innerY + "px";
+        animal.style.left = touches.pageX - innerX + "px";
+        animal.style.top = touches.pageY - innerY + "px";
     } else {
         // for desktop
-        e.clientX && (box.style.left = e.clientX - innerX + "px");
-        e.clientY && (box.style.top = e.clientY - innerY + "px");
+        e.clientX && (animal.style.left = e.clientX - innerX + "px");
+        e.clientY && (animal.style.top = e.clientY - innerY + "px");
     }
 }
 
@@ -45,15 +61,69 @@ function handleEnd() {
     setOverscroll("auto");
 }
 
+// prevents "pull down refresh" while dragging on mobile
 function setOverscroll(value) {
     const body = document.body,
         htmlElem = document.querySelector("html");
     [body, htmlElem].forEach((elem) => (elem.style.overscrollBehavior = value));
 }
 
+// get the clicked coordinates relative to the animal
+// in order to calculate a smooth dragging experience
 function setInnerCoords(e) {
     const touches = getTouches(e),
         rect = e.target.getBoundingClientRect();
     innerX = (touches?.pageX || e.clientX) - rect.left;
     innerY = (touches?.pageY || e.clientY) - rect.top;
+}
+
+function moveAnimal(elem, ms) {
+    const moveUp = Math.random() < 0.5,
+        moveLeft = Math.random() < 0.5,
+        cage = document.querySelector("#cage");
+    let interval = setInterval(() => {
+        elem.style.top =
+            elem.offsetTop + (moveUp ? -1 : 1) * Math.random() + "px";
+        elem.style.left =
+            elem.offsetLeft + (moveLeft ? -1 : 1) * Math.random() + "px";
+        const hasEscaped =
+                !animals.length ||
+                animals.find((animal) => {
+                    const escapedLeft =
+                            animal.offsetLeft + animal.clientWidth <
+                            cage.offsetLeft,
+                        escapedRight =
+                            animal.offsetLeft >
+                            cage.offsetLeft + cage.clientWidth,
+                        escapedTop =
+                            animal.offsetTop + animal.clientHeight <
+                            cage.offsetTop,
+                        escapedBotom =
+                            animal.offsetTop >
+                            cage.offsetTop + cage.clientHeight;
+                    return (
+                        escapedLeft ||
+                        escapedRight ||
+                        escapedTop ||
+                        escapedBotom
+                    );
+                }),
+            isLost =
+                elem.offsetTop + elem.clientHeight < 0 ||
+                elem.offsetLeft + elem.clientWidth < 0 ||
+                elem.offsetTop > window.innerHeight ||
+                elem.offsetLeft > window.innerWidth;
+        cage.style.backgroundColor = hasEscaped ? "lightsalmon" : "yellow";
+        if (isLost) {
+            alert("You lost an animal!");
+            elem.remove();
+            clearInterval(interval);
+            animals = animals.filter((animal) => animal !== elem);
+        }
+        if (!gameIsOver && !animals.length) {
+            gameIsOver = true;
+            alert("GAME OVER");
+        }
+    }, ms);
+    return interval;
 }
